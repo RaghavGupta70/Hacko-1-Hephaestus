3494151edb9628229835871f8fdab085e8a32419
RaghavGupta70 <55792366+RaghavGupta70@users.noreply.github.com>
2021-03-27 16:08:29 +0530

    .
    Flutter Fire

--- hephaestapp/lib/net/flutterfire.dart
+++ hephaestapp/lib/net/flutterfire.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hephaestapp/model/user.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -20,7 +19,7 @@ class AuthService {
       User firebaseuser = result.user;
       return _userfromfirebaseUser(firebaseuser);
      } on FirebaseAuthException catch (e) {
-       Fluttertoast.showToast(msg: e.message, gravity: ToastGravity.TOP,timeInSecForIosWeb: 5,);
+       Fluttertoast.showToast(msg: e.message, gravity: ToastGravity.TOP,timeInSecForIosWeb: 5,textColor: Colors.deepPurple, backgroundColor: Colors.deepPurple[100]);
  }
     
      catch (e) {
@@ -38,14 +37,87 @@ class AuthService {
       User firebaseuser = result.user;
       return _userfromfirebaseUser(firebaseuser);
     } on FirebaseAuthException catch (e) {
-   Fluttertoast.showToast(msg: e.message, gravity: ToastGravity.TOP,timeInSecForIosWeb: 5,);
+   Fluttertoast.showToast(msg: e.message, gravity: ToastGravity.TOP,timeInSecForIosWeb: 5,textColor: Colors.deepPurple, backgroundColor: Colors.deepPurple[100]);
  }
     catch (e) {
       print(e.toString());
       return null;
     }
   }
+
+Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
 }
+
+Future <bool> addCourse(String courseid) async {
+String uid = FirebaseAuth.instance.currentUser.uid;
+DocumentReference documentReference = FirebaseFirestore.instance.collection('Users').doc(uid).collection('Courses').doc(courseid);
+FirebaseFirestore.instance.runTransaction((transaction) async{
+DocumentSnapshot snapshot = await transaction.get(documentReference);
+  documentReference.set({'Course': courseid});
+  return true;
+});
+
+}
+
+Future<bool> removeCourse(String courseid) async{
+String uid = FirebaseAuth.instance.currentUser.uid;
+FirebaseFirestore.instance.collection('Users').doc(uid).collection('Courses').doc(courseid).delete();
+return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Future<bool> signIn(String email, String password) async {
+  try{
+
+    await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+    return true;
+  }
+  catch(e){
+    print(e);
+    return false;
+  }
+}
+
+Future<bool> register(String email, String password) async {
+ try{
+   await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+  return true;
+ } on FirebaseAuthException catch (e) {
+   if (e.code == 'weak-password') {
+     print('The passsword provided is too weak.');
+   } else if ( e.code == 'email-already-in-use') {
+     print('The account already exists for that email.');
+   }
+   return false;
+ } catch (e) {
+   print(e.toString());
+   return false;
+ }
+}
+
 bool getCurrentUser()
 {
   if(FirebaseAuth.instance.currentUser != null){
@@ -54,7 +126,8 @@ bool getCurrentUser()
 return false;
 }
 
-Future<void> signOut() async
+void signOut()
 {
   FirebaseAuth.instance.signOut();
+  
 }
